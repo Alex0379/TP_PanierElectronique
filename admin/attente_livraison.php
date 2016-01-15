54e9eef7c19fc2377888bf275a80fd17e53b7d89
Alex0379 <alex.pacaud.79@gmail.com>
2016-01-15 10:20:53 +0100

    Rangement du bordel

---
+++ admin/attente_livraison.php
@@ -0,0 +1,65 @@
+<?php include_once('header_admin.html'); ?>
+
+        <div class="conteneur">
+            <div class="header" style="text-align: center;">
+				<h1>Commandes en attente de livraison</h1>
+			</div>
+            <div class="panier">
+                <table class=" articlesPanier mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                    <thead>
+                        <tr>
+                            <th>N° commande</th>
+                            <th>Date</th>
+                            <th>id_utilisateur</th>
+                            <th>N° article</th>
+                            <th>Quantité</th>
+                            <th class="mdl-data-table__cell--non-numeric">Etat</th>
+                            <th class="mdl-data-table__text-center">Expédier</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+                            <td class="mdl-data-table__text-center">
+                                <button id="tt1" class="mdl-button mdl-js-button mdl-button--icon">
+                                    <i class="material-icons">send</i>
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+                            <td class="mdl-data-table__text-center">
+                                <button id="tt1" class="mdl-button mdl-js-button mdl-button--icon">
+                                    <i class="material-icons">send</i>
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td>$2.90</td>
+                            <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+                            <td class="mdl-data-table__text-center">
+                                <button id="tt1" class="mdl-button mdl-js-button mdl-button--icon">
+                                    <i class="material-icons">send</i>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+<?php include_once('footer_admin.html'); ?>
